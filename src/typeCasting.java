40d757f36261f17ac03b93782172cfe987829795
Gopal Krishna <gopal327.be22@chitkara.edu.in>
2024-08-17 16:16:08 +0530

    first

--- src/typeCasting.java
+++ src/typeCasting.java
@@ -1,5 +1,17 @@
 public class typeCasting {
     public static void main(String[] args) {
+        int num = (int)(578.84);
+        System.out.println(num);
+//        byte a = (byte)(127);
+//        System.out.println(a);
+        byte a=20;
+        byte b = 30;
+        byte c = 50;
+//        byte d = a*b + c;
+        int d = a*b + c;
+        System.out.println(d);
+        byte e =50;
+//        e=e*10;
 
 
     }
