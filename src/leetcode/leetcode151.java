e6d65592754f3eab500c70fbe48c591ee803d5f3
Gopal Krishna <gopal327.be22@chitkara.edu.in>
2024-09-17 21:06:26 +0530

    java

--- src/leetcode/leetcode151.java
+++ src/leetcode/leetcode151.java
@@ -1,11 +1,33 @@
 package leetcode;
 
+import java.util.*;
 public class leetcode151 {
     public static void main(String[] args) {
-        String str="  ab  cd  ";
-        String s=str.trim();
-        System.out.println(str);
-        System.out.println(s);
+        String s="a good   example";
+        String str=s.trim();
+        List<String> arr=new ArrayList<>();
+        String temp="";
 
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+            if(ch!=' '){
+                temp+=ch;
+            }
+            if(ch==' ') {
+                if(temp.length()>0){
+                    arr.add(temp.trim());
+
+                }
+                temp="";
+            }
+        }
+        if(temp.length()>0) arr.add(temp);
+        String ans="";
+        for(int i=arr.size()-1;i>=0;i--){
+            System.out.println(arr.get(i));
+            ans+=arr.get(i);
+            if(i!=0) ans+=' ';
+        }
+        System.out.println(ans);
     }
 }
--- src/leetcode/leetcode84.java
+++ src/leetcode/leetcode84.java
@@ -1,4 +1,66 @@
 package leetcode;
 
+import java.util.*;
+
 public class leetcode84 {
+    public static void main(String[] args) {
+        String s1="Luthra bhen ka loda hai";
+        String s2="Luthra bhen ka pakoda hai";
+        ArrayList<String> arr1=new ArrayList<>();
+        ArrayList<String> arr2=new ArrayList<>();
+        String temp="";
+        for(int i=0;i<s1.length();i++){
+            char ch=s1.charAt(i);
+            if(ch==' '){
+                arr1.add(temp);
+                temp="";
+            }
+            temp+=ch;
+        }
+        if(temp.length()>0) arr1.add(temp);
+        temp="";
+        for(int i=0;i<s2.length();i++){
+            char ch=s2.charAt(i);
+            if(ch==' '){
+                arr2.add(temp);
+                temp="";
+            }
+            temp+=ch;
+        }
+        if(temp.length()>0) arr2.add(temp);
+
+        System.out.println(arr1.toString());
+        System.out.println(arr2.toString());
+
+        Map<String,Integer> mp=new HashMap<>();
+
+        for(String item:arr1){
+            if(mp.containsKey(item)){
+                mp.put(item,mp.get(item)+1);
+            }
+            else{
+                mp.put(item,1);
+            }
+        }
+        for(String item:arr2){
+            if(mp.containsKey(item)){
+                mp.put(item,mp.get(item)+1);
+            }
+            else{
+                mp.put(item,1);
+            }
+        }
+        String[] ans=new String[2];
+        int i=0;
+        for(Map.Entry<String,Integer> entry:mp.entrySet()){
+            if(entry.getValue()==1) {
+                if(entry.getKey().trim()!=null){
+                    ans[i]=(entry.getKey().trim());
+                    i++;
+                }
+
+            }
+        }
+        System.out.println(ans);
+    }
 }
